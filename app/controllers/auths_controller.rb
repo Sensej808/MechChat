fb6140bfebc76b552f5097b53c78e306193f4e94
SskytT <regmail21@yandex.ru>
2023-11-30 03:13:38 +0300

    begin auth

--- app/controllers/auths_controller.rb
+++ app/controllers/auths_controller.rb
@@ -1,12 +1,14 @@
 class AuthsController < ActionController::Base
   def create
-    #@user = User.find_by email: auth_params[:email]
-    #@user = User.where([:email, :pwd] auth_params[:email], pwd: auth_params[:pwd]).take
-    #if (@user.nil?)
-    #  render 'login'
-    #else
-    #  redirect_to "http://google.com", allow_other_host: true
-    #end
+    @user = User.where(:email => auth_params[:email], :pwd => auth_params[:pwd]).take
+    if (@user.nil?)
+      render 'login'
+    else
+      session[:id] = @user[:id]
+      #render "users/profiles"
+      redirect_to "http://google.com", allow_other_host: true
+      puts session[:id]
+    end
   end
 
   def delete
