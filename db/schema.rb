5bcac41af95d85471442ca3eef8e9a0b87c68cd5
Sensej808 <sasha-tkachenko-03@mail.ru>
2023-12-01 03:07:49 +0300

    fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_08_224809) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_232131) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,7 +24,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_08_224809) do
   end
 
   create_table "chats", force: :cascade do |t|
-    t.integer "members", null: false
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -50,7 +49,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_08_224809) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "members", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
